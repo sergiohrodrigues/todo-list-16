f638b1139999d630155795194861f28d450e5a0b
Sergio <sergiohrodriguess@gmail.com>
2025-02-03 15:14:56 -0300

    add service signals

---
+++ src/app/services/todo-signals.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, signal } from '@angular/core';
+import { Todo } from '../models/model/todo.model';
+import { TodoKeyLocalStorage } from '../models/enum/TodoKeyLocalStorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoSignalsService {
+  public todosState = signal<Array<Todo>>([]);
+
+  public updateTodos({ id, title, description, done }: Todo): void {
+    if((id && title && description !== null) || undefined ){
+      this.todosState.mutate((todos) => {
+        if(todos !== null){
+          todos.push(new Todo(id, title, description, done));
+        }
+      })
+    }
+  }
+
+  public saveTodoInLocalStorage(): void {
+    const todos = JSON.stringify(this.todosState());
+    localStorage.setItem(TodoKeyLocalStorage.TODO_LIST, todos);
+  }
+
+}
