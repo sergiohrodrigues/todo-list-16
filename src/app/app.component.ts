8f415eb82fab6c9a372325765df34ff707b71cf9
Sergio <sergiohrodriguess@gmail.com>
2025-02-04 15:40:04 -0300

    Operator SwitchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { SchoolData, SchoolService } from './services/school.service';
-import { from, map, Observable, of, Subject, takeUntil, zip } from 'rxjs';
+import { filter, from, map, Observable, of, Subject, switchMap, takeUntil, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -25,11 +25,16 @@ export class AppComponent implements OnInit {
   private readonly destroy$: Subject<void> = new Subject();
 
   private ages = of(20, 30, 40, 50, 60, 70);
+
   private peoplesDatas = from([
     { name: 'Marcos Junior', idade : 20, profession: 'Software Developer' },
     { name: 'Sergio Rodrigues', idade : 30, profession: 'Fullstack Developer' },
     { name: 'Thiago Queiroz', idade : 21, profession: 'Backend Developer' },
+    { name: 'Joao Queiroz', idade : 21, profession: 'Software Developer' },
+    { name: 'Erik Queiroz', idade : 21, profession: 'Software Developer' },
   ])
+
+  private studentUserId = '2';
 
   private zipSchoolResponse$ = zip(
     this.getStudentsDatas(),
@@ -39,7 +44,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // this.getSchoolDatas();
     // this.getMultiplieAges();
-    this.getPeopleProfessions()
+    // this.getPeopleProfessions()
+    // this.getSoftwareDeveloperNames()
+    this.handleFindStudentsById();
+  }
+
+  handleFindStudentsById() {
+    this.getStudentsDatas()
+      .pipe(
+        switchMap((students) => this.findStudentsById(students, this.studentUserId))
+      )
+      .subscribe({
+        next: (response) => {
+          console.log('RETORNO ESTUDANTE FILTRADO', response)
+        }
+      })
+  }
+
+  public findStudentsById( students: Array<SchoolData>, userId: string ){
+    return of(students.find((student) => student.id === userId))
   }
 
   getMultiplieAges() {
@@ -62,13 +85,27 @@ export class AppComponent implements OnInit {
 
   getSchoolDatas(): void {
     this.zipSchoolResponse$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (response) => {
-          console.log('STUDENTS', response[0])
-          console.log('TEACHERS', response[1])
-        }
-      })
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        console.log('STUDENTS', response[0])
+        console.log('TEACHERS', response[1])
+      }
+    })
+  }
+
+
+  getSoftwareDeveloperNames(): void {
+    this.peoplesDatas
+    .pipe(
+      filter((people) => people.profession === "Software Developer"),
+      map((people) => people.name)
+    )
+    .subscribe({
+      next: (response) => {
+        console.log('NOME DO DESENVOLVEDOR', response)
+      }
+    })
   }
 
   getStudentsDatas(): Observable<Array<SchoolData>> {
