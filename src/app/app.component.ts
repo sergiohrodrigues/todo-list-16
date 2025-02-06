1712fd0f8e89d902a89466bcff8a00518efe1e2a
Sergio <sergiohrodriguess@gmail.com>
2025-02-06 10:23:15 -0300

    project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,104 +21,104 @@ import { Todo } from './models/model/todo.model';
 })
 
 export class AppComponent
-//implements OnInit
+implements OnInit
 {
-  // private schoolService = inject(SchoolService);
-  // public students: Array<SchoolData> = [];
-  // public teachers: Array<SchoolData>= [];
-  // private readonly destroy$: Subject<void> = new Subject();
+  private schoolService = inject(SchoolService);
+  public students: Array<SchoolData> = [];
+  public teachers: Array<SchoolData>= [];
+  private readonly destroy$: Subject<void> = new Subject();
 
-  // private ages = of(20, 30, 40, 50, 60, 70);
+  private ages = of(20, 30, 40, 50, 60, 70);
 
-  // private peoplesDatas = from([
-  //   { name: 'Marcos Junior', idade : 20, profession: 'Software Developer' },
-  //   { name: 'Sergio Rodrigues', idade : 30, profession: 'Fullstack Developer' },
-  //   { name: 'Thiago Queiroz', idade : 21, profession: 'Backend Developer' },
-  //   { name: 'Joao Queiroz', idade : 21, profession: 'Software Developer' },
-  //   { name: 'Erik Queiroz', idade : 21, profession: 'Software Developer' },
-  // ])
+  private peoplesDatas = from([
+    { name: 'Marcos Junior', idade : 20, profession: 'Software Developer' },
+    { name: 'Sergio Rodrigues', idade : 30, profession: 'Fullstack Developer' },
+    { name: 'Thiago Queiroz', idade : 21, profession: 'Backend Developer' },
+    { name: 'Joao Queiroz', idade : 21, profession: 'Software Developer' },
+    { name: 'Erik Queiroz', idade : 21, profession: 'Software Developer' },
+  ])
 
-  // private studentUserId = '2';
+  private studentUserId = '2';
 
-  // private zipSchoolResponse$ = zip(
-  //   this.getStudentsDatas(),
-  //   this.getTeachersDatas()
-  // );
+  private zipSchoolResponse$ = zip(
+    this.getStudentsDatas(),
+    this.getTeachersDatas()
+  );
 
-  // ngOnInit(): void {
-  //   // this.getSchoolDatas();
-  //   // this.getMultiplieAges();
-  //   // this.getPeopleProfessions()
-  //   // this.getSoftwareDeveloperNames()
-  //   this.handleFindStudentsById();
-  // }
+  ngOnInit(): void {
+    // this.getSchoolDatas();
+    // this.getMultiplieAges();
+    // this.getPeopleProfessions()
+    // this.getSoftwareDeveloperNames()
+    //this.handleFindStudentsById();
+  }
 
-  // handleFindStudentsById() {
-  //   this.getStudentsDatas()
-  //     .pipe(
-  //       switchMap((students) => this.findStudentsById(students, this.studentUserId))
-  //     )
-  //     .subscribe({
-  //       next: (response) => {
-  //         console.log('RETORNO ESTUDANTE FILTRADO', response)
-  //       }
-  //     })
-  // }
+  handleFindStudentsById() {
+    this.getStudentsDatas()
+      .pipe(
+        switchMap((students) => this.findStudentsById(students, this.studentUserId))
+      )
+      .subscribe({
+        next: (response) => {
+          console.log('RETORNO ESTUDANTE FILTRADO', response)
+        }
+      })
+  }
 
-  // public findStudentsById( students: Array<SchoolData>, userId: string ){
-  //   return of(students.find((student) => student.id === userId))
-  // }
+  public findStudentsById( students: Array<SchoolData>, userId: string ){
+    return of(students.find((student) => student.id === userId))
+  }
 
-  // getMultiplieAges() {
-  //   this.ages.pipe(map((age) => age * 2)).subscribe({
-  //     next: (response) => {
-  //       console.log(response)
-  //     }
-  //   })
-  // }
+  getMultiplieAges() {
+    this.ages.pipe(map((age) => age * 2)).subscribe({
+      next: (response) => {
+        console.log(response)
+      }
+    })
+  }
 
-  // getPeopleProfessions(): void {
-  //   this.peoplesDatas
-  //   .pipe(map((people) => people.profession))
-  //   .subscribe({
-  //     next: (response) => {
-  //       console.log('PROFISSÃO ', response)
-  //     }
-  //   })
-  // }
+  getPeopleProfessions(): void {
+    this.peoplesDatas
+    .pipe(map((people) => people.profession))
+    .subscribe({
+      next: (response) => {
+        console.log('PROFISSÃO ', response)
+      }
+    })
+  }
 
-  // getSchoolDatas(): void {
-  //   this.zipSchoolResponse$
-  //   .pipe(takeUntil(this.destroy$))
-  //   .subscribe({
-  //     next: (response) => {
-  //       console.log('STUDENTS', response[0])
-  //       console.log('TEACHERS', response[1])
-  //     }
-  //   })
-  // }
+  getSchoolDatas(): void {
+    this.zipSchoolResponse$
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        console.log('STUDENTS', response[0])
+        console.log('TEACHERS', response[1])
+      }
+    })
+  }
 
 
-  // getSoftwareDeveloperNames(): void {
-  //   this.peoplesDatas
-  //   .pipe(
-  //     filter((people) => people.profession === "Software Developer"),
-  //     map((people) => people.name)
-  //   )
-  //   .subscribe({
-  //     next: (response) => {
-  //       console.log('NOME DO DESENVOLVEDOR', response)
-  //     }
-  //   })
-  // }
+  getSoftwareDeveloperNames(): void {
+    this.peoplesDatas
+    .pipe(
+      filter((people) => people.profession === "Software Developer"),
+      map((people) => people.name)
+    )
+    .subscribe({
+      next: (response) => {
+        console.log('NOME DO DESENVOLVEDOR', response)
+      }
+    })
+  }
 
-  // getStudentsDatas(): Observable<Array<SchoolData>> {
-  //   return this.schoolService.getStudents();
-  // }
+  getStudentsDatas(): Observable<Array<SchoolData>> {
+    return this.schoolService.getStudents();
+  }
 
-  // getTeachersDatas(): Observable<Array<SchoolData>> {
-  //   return this.schoolService.getTeachers();
-  // }
+  getTeachersDatas(): Observable<Array<SchoolData>> {
+    return this.schoolService.getTeachers();
+  }
 
 
   @Input() public projectName!: string;
